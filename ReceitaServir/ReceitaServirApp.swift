0d2bb29a70828131f002fd407bd6be90c3bc990e
Edilson Schwanck <edilsonborges96@gmail.com>
2023-07-24 15:38:00 -0300

    Initial Commit

---
+++ ReceitaServir/ReceitaServirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceitaServirApp.swift
+//  ReceitaServir
+//
+//  Created by Edilson Borges on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ReceitaServirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
